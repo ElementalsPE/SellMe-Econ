fbcc1db4450fb534733f4821523276ab03880972
cosmicnebula200 <cosmicnebula25@gmail.com>
2022-07-21 23:45:03 +0530

    Compatibility with Wands

--- src/cosmicnebula200/SellMe/Utils.php
+++ src/cosmicnebula200/SellMe/Utils.php
@@ -22,7 +22,7 @@ class Utils
         return true;
     }
 
-    public function sellChest(Player $player, Chest $chest, int $multiplier = 1): int
+    public function sellChest(Chest $chest, int $multiplier = 1): int
     {
         $inv = $chest->getInventory();
         $amount = 0;
